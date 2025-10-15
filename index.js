3589b7e30ca14eb786bfa74334ae11eb1308ec48
Bhavya KB <bhavyabhavadas21@gmail.com>
2025-10-15 12:36:59 +0530

    index added

---
+++ index.js
@@ -0,0 +1,35 @@
+//1. import json server
+
+const JsonServer = require ('json-server')
+
+//2. create server for running josn file
+
+const rBuilder =  JsonServer.create()
+
+//3. create middleware
+
+const middleware = JsonServer.defaults()
+
+// 6. import db.json file
+
+const router = JsonServer.router("db.json")
+
+// 4. define port to run the server
+
+const PORT = 3000 || process.env.PORT
+
+// 5. use middleware
+
+rBuilder.use(middleware)
+
+// 7. use router
+
+rBuilder.use(router)
+
+// 8. tell the server to listen for client request
+
+rBuilder.listen(PORT,()=>{
+    console.log(`Resume builder started at PORT number ${PORT}`);
+    
+})
+
